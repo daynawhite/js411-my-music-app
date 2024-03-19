4d1a10482d76dccaa9d0b00a95ce27be3db4268c
daynawhite <dayna26.2@gmail.com>
2024-03-18 20:39:48 -0500

    assignment completed with all required functionality

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { Card, Box, CardContent, Typography } from '@mui/material';
-import OnlineSwitch from './Switch';
+import { useState } from 'react';
+import { Card, Box, CardContent, Typography, Switch, Slider, NativeSelect } from '@mui/material';
+import { alpha, styled } from '@mui/material/styles';
+import { pink } from '@mui/material/colors';
 import '../App.css';
-import VolumeSlider from './VolumeSlider';
-import QualitySelector from './SoundQuality';
-// import Notifications from './Notifications';
 
-
-  
+ 
 
 const Dashboard = () => {
-  const [quality, setQuality] = useState(2);
+ 
   const [onlineStatus, setOnlineStatus] = useState(false);
-  const [volume, setVolume] = useState(20);
+  const PinkSwitch = styled(Switch)(({ theme }) => ({
+    '& .MuiSwitch-switchBase.Mui-checked': {
+      color: pink[600],
+      '&:hover': {
+        backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
+      },
+    },
+    '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+      backgroundColor: pink[600],
+    },
+  }));
 
     const onlineModeCard = (
         <React.Fragment>
@@ -24,10 +31,20 @@ const Dashboard = () => {
             <Typography sx={{ fontSize: 12, mb: 1.5 }} color="text.secondary">
               Is the application connected to the internet?
             </Typography>
-            <OnlineSwitch onlineStatus={onlineStatus} setOnlineStatus={setOnlineStatus}/>
+            <PinkSwitch 
+            checked={onlineStatus}
+            onChange={()=> setOnlineStatus(!onlineStatus)} 
+            inputProps={{ 'aria-label': 'controlled' }}
+             />
           </CardContent>
         </React.Fragment>
       );
+
+    const [volume, setVolume] = useState(20);
+    const handleSliderChange = (event, newValue) => {
+      setVolume(newValue);
+      };
+
 
       const masterVolumeCard = (
         <React.Fragment>
@@ -38,10 +55,24 @@ const Dashboard = () => {
             <Typography sx={{ fontSize: 12, mb: 1.5 }} color="text.secondary">
               Overrides all other sound settings in this application
             </Typography>
-            <VolumeSlider volume={volume} setVolume={setVolume}/>
+            <Slider 
+                aria-label="Volume" 
+                value={volume} 
+                onChange={handleSliderChange} 
+                shiftStep={10}
+                min={0}
+                max={100} />
           </CardContent>
         </React.Fragment>
         );
+
+    const [soundQuality, setSoundQuality] = useState(2);
+    const handleQualityChange = (event) => {
+      const {
+        target: { value },
+        } = event;
+        setSoundQuality(value);
+      };
 
     const soundQualityCard = (
         <React.Fragment>
@@ -52,40 +83,20 @@ const Dashboard = () => {
             <Typography sx={{ fontSize: 12, mb: 1.5 }} color="text.secondary">
             Manually control the music quality in event of poor connection
             </Typography>
-            <QualitySelector quality={quality} setQuality={setQuality}/>
+            <NativeSelect
+              onChange={handleQualityChange}
+              defaultValue={soundQuality}
+              inputProps={{
+                id: 'uncontrolled-native',
+              }}
+              >
+              <option value={1}>Low</option>
+              <option value={2}>Normal</option>
+              <option value={3}>High</option>
+            </NativeSelect>
         </CardContent>
         </React.Fragment>
         );
-
-        const Notifications = () => {
-          const [notifications, setNotifications] = useState([]);
-          
-            // This effect will run whenever `quality`, 'onlineStatus', and/or 'volume' changes
-          useEffect(() => {
-              // Perform any operations to update `notifications` based on the new `quality`, 'onlineStatus', and/or 'volume' values
-              if (!onlineStatus) {notifications.push("Your application is offline. You won't be able to share or stream music to other devices.")};
-              if (volume > 80) {notifications.push('Listening to music at a high volume could cause long-term hearing loss.')};
-              if (quality === '1') {notifications.push('Music quality is degraded. Increase quality if your connection allows it.')};
-                
-            setNotifications(notifications);
-        
-            return () => {
-              setNotifications([])
-            };
-        
-            },
-             [
-              // quality, onlineStatus, volume, 
-              notifications]
-             );
-          
-          
-            // return (
-            // <ul>
-            //  {notifications.map((notification, index) => <li key={index}>{notification}</li>)}
-            // </ul>
-            // )
-          };
 
     return (
       <div>
@@ -95,19 +106,19 @@ const Dashboard = () => {
 
             <div className="online-mode-card">
             <Box sx={{ width: 200, height: 200 }}>
-                <Card variant="outlined">{onlineModeCard}</Card>
+                <Card sx={{ width: 200, minHeight: 170 }} variant="outlined">{onlineModeCard}</Card>
             </Box>
             </div>
 
             <div className="master-volume-card">
-            <Box sx={{ width: 200, height: 200 }}>
-                <Card className="card-content" variant="outlined">{masterVolumeCard}</Card>
+            <Box sx={{ width: '200px', height: '200px' }}>
+                <Card sx={{ width: 200, minHeight: 170 }} className="card-content" variant="outlined">{masterVolumeCard}</Card>
             </Box>
             </div>
 
             <div className="sound-quality-card">
             <Box sx={{ width: 200, height: 200 }}>
-                <Card variant="outlined">{soundQualityCard}</Card>
+                <Card sx={{ width: 200, minHeight: 170 }} variant="outlined">{soundQualityCard}</Card>
             </Box>
             </div>
 
@@ -116,7 +127,10 @@ const Dashboard = () => {
         <h2>System Notifications:</h2>
        
         <ul>
-          {notifications.map((notification, index) => <li key={index}>{notification}</li>)}
+          {!onlineStatus && <li>Your application is offline. You won't be able to share or stream music to other devices.</li>}
+          {volume > 80 && <li>Listening to music at a high volume could cause long-term hearing loss.</li>}
+          {soundQuality === '1' && <li>Music quality is degraded. Increase quality if your connection allows it.</li>}
+        
         </ul>
        
       </div>
